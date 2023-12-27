6f7b6644b14e37b050669470c2f7cbf94b647c79
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-12-27 18:53:26 +0530

    add change password

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -7,10 +7,24 @@ import { useEffect, useState } from "react";
 function UserProfile() {
   // Redirect away if NOT auth
 
+  async function changePasswordHandler(passwordData){
+ const responce = await fetch(`/api/user/change-password`,{
+      method:"PATCH",
+      body:JSON.stringify(passwordData),
+      headers:{
+        'Content-Type':"application/json"
+      }
+    });
+
+    const data = await responce.json()
+    console.log(data)
+
+  }
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm  onChangePassword={changePasswordHandler}/>
     </section>
   );
 }
